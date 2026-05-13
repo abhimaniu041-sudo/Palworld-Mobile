92e76805c7d40a5592f108184640ee5dbae6d27c
abhimaniu041-sudo <abhimaniu041@gmail.com>
2026-05-14 01:47:50 +0530

    Create Items.js

---
+++ src/data/Items.js
@@ -0,0 +1,17 @@
+// Game: Palworld Mobile - Item Database
+export const Items = {
+    BERRY: {
+        id: 'red_berry',
+        name: 'Red Berry',
+        nutrition: 15,
+        type: 'FOOD',
+        icon: '🍓'
+    },
+    MEAT: {
+        id: 'raw_meat',
+        name: 'Raw Meat',
+        nutrition: 30,
+        type: 'FOOD',
+        icon: '🍖'
+    }
+};
